41707bde8d3f6a307d21e458737338e5df536563
stanislav.sardarian <stanislavsardarian@larixon.com>
2026-02-25 16:23:47 +0100

    feat: added ship movement

--- main.py
+++ main.py
@@ -16,11 +16,18 @@ fighter_width, fighter_height = fighter_image.get_size()
 
 fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_height
 
+FIGHTER_STEP = 20
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and fighter_x >= FIGHTER_STEP:
+                fighter_x -= FIGHTER_STEP
+            if event.key == pygame.K_RIGHT and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
+                fighter_x += FIGHTER_STEP
+
 
     screen.fill(screen_fill_color)
     screen.blit(fighter_image, (fighter_x, fighter_y))
