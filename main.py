7719acd36b453e20cc22dc95a4504c2401e27c34
stanislav.sardarian <stanislavsardarian@larixon.com>
2026-02-25 16:16:21 +0100

    feat: cosmic shooter init

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
 import sys
 import pygame
 
-clock = pygame.time.Clock()
 pygame.init()
 
-
 screen_width, screen_height = 800, 600
+screen_fill_color = (32, 52, 71)
+
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-pygame.display.set_caption("My Pygame")
-fill_color = (32, 52, 71)
+pygame.display.set_caption("Cosmic Shooter")
 
-rect_width, rect_height = 100, 200
-rect_x = screen_width / 2 - rect_width / 2
-rect_y = screen_height / 2 - rect_height / 2
-rect_color = pygame.Color("lightyellow")
+fighter_image = pygame.image.load("images/fighter.png")
 
-STEP = 10
+fighter_width, fighter_height = fighter_image.get_size()
+
+fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_height
 
 
 while True:
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
             sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and rect_y >= STEP:
-                rect_y -= STEP
-            if event.key == pygame.K_DOWN and rect_y <= screen_height - rect_height - STEP:
-                rect_y += STEP
-            if event.key == pygame.K_LEFT and rect_x >= STEP:
-                rect_x -= STEP
-            if event.key == pygame.K_RIGHT and rect_x <= screen_width - rect_width - STEP:
-                rect_x += STEP
 
-
-    screen.fill(fill_color)
-    pygame.draw.rect(screen, rect_color, (rect_x, rect_y, rect_width, rect_height))
+    screen.fill(screen_fill_color)
+    screen.blit(fighter_image, (fighter_x, fighter_y))
     pygame.display.update()
