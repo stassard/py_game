6df777e6857b3fe0a5bf25bf7405728907af7f71
stanislav.sardarian <stanislavsardarian@larixon.com>
2026-03-04 16:27:34 +0100

    feat: added alien

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from random import randint
 
 pygame.init()
 
@@ -10,20 +11,27 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Cosmic Shooter")
 
+FIGHTER_STEP = 0.5
 fighter_image = pygame.image.load("images/fighter.png")
-
 fighter_width, fighter_height = fighter_image.get_size()
+fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_height
+fighter_is_moving_left, fighter_is_moving_right = False, False
+
+
+BALL_STEP = 0.3
 rocket_image = pygame.image.load("images/rocket.png")
 rocket_width, rocket_height = rocket_image.get_size()
-
-
-fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_height
 rocket_x, rocket_y = 0, 0
 rocket_was_fired = False
 
-FIGHTER_STEP = 0.5
-BALL_STEP = 0.3
-fighter_is_moving_left, fighter_is_moving_right = False, False
+ALIEN_STEP = 0.1
+alien_image = pygame.image.load("images/alien.png")
+alien_width, alien_height = alien_image.get_size()
+alien_x, alien_y = randint(0, screen_width - alien_width), 0
+
+
+
+
 
 while True:
     for event in pygame.event.get():
@@ -50,6 +58,8 @@ while True:
     if fighter_is_moving_right and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
         fighter_x += FIGHTER_STEP
 
+    alien_y += ALIEN_STEP
+
     if rocket_was_fired and rocket_y + rocket_height < 0:
         rocket_was_fired = False
 
@@ -60,6 +70,7 @@ while True:
 
     screen.fill(screen_fill_color)
     screen.blit(fighter_image, (fighter_x, fighter_y))
+    screen.blit(alien_image, (alien_x, alien_y))
 
     if rocket_was_fired:
         screen.blit(rocket_image, (rocket_x, rocket_y))
